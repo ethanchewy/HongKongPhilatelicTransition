096b058b339d9d94513160dcadb836aaaaf49d35
Ethan Chiu <ethanc@Ethans-MacBook-Air.local>
2015-08-20 05:43:37 -0500

    Took out unneccessary code
    - Took out unnecssary files from folders

--- js/agency.js
+++ js/agency.js
@@ -1,21 +1,3 @@
-/*!
- * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
- * Code licensed under the Apache License v2.0.
- * For details, see http://www.apache.org/licenses/LICENSE-2.0.
- */
-/*$(function() {
-  $('#loading-indicator').hide();  // hide it initially.
-  $(document)  
-    .ajaxStart(function() {
-      $('#loading-indicator').show(); // show on any Ajax event.
-    })
-    .ajaxStop(function() {
-      $('#loading-indicator').hide(); // hide it when it is done.
-  });
-}); */
-
-
-
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
     target: '.navbar-fixed-top'
